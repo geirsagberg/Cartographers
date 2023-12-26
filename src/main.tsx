a85623a1e551ee991f680a74f21994983bdb479e
Geir Sagberg <geir.sagberg@gmail.com>
2023-12-26 15:36:58 +0100

    Confirm refresh

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './normalize.css'
 
+if (import.meta.env.MODE !== 'development') {
+  window.onbeforeunload = () => 'Are you sure you want to leave?'
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
